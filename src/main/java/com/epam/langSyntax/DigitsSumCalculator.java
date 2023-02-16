2bec6f0134ad9b54e409cb94a688cec2bf2112ea
sergey3321 <mail.sergey21@gmail.com>
2023-02-16 16:09:56 +0300

    java.stage0.masterTask

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,22 +3,23 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
-        String sumNumbers1 = String.valueOf(number);
-        char extractSimbol1 = sumNumbers1.charAt(0);
-        String valueSimbol1 = String.valueOf(extractSimbol1);
-        int sumSimbol1 = Integer.parseInt(valueSimbol1);
-        String sumNumbers2 = String.valueOf(number);
-        char extractSimbol2 = sumNumbers2.charAt(1);
-        String valueSimbol2 = String.valueOf(extractSimbol1);
-        int sumSimbol2 = Integer.parseInt(valueSimbol1);
-        char extractSimbol3 = sumNumbers2.charAt(2);
-        String valueSimbol3 = String.valueOf(extractSimbol1);
-        int sumSimbol3 = Integer.parseInt(valueSimbol1);
-        char extractSimbol4 = sumNumbers2.charAt(3);
-        String valueSimbol4 = String.valueOf(extractSimbol1);
-        int sumSimbol4 = Integer.parseInt(valueSimbol1);
-        int sumSimbol5 = sumSimbol1 + sumSimbol2 + sumSimbol3 + sumSimbol4;
-        System.out.println(sumSimbol5);
+        String forConvertNumber1 = String.valueOf(number);
+        char extractNumber1 = forConvertNumber1.charAt(0);
+        String valueSimbol1 = String.valueOf(extractNumber1);
+        int forSumNumber1 = Integer.parseInt(valueSimbol1);
+        String forConvertNumber2 = String.valueOf(number);
+        char extractSimbol2 = forConvertNumber2.charAt(1);
+        String valueSimbol2 = String.valueOf(extractSimbol2);
+        int forSumNumber2 = Integer.parseInt(valueSimbol2);
+        String forConvertNumbers3 = String.valueOf(number);
+        char extractSimbol3 = forConvertNumbers3.charAt(2);
+        String valueSimbol3 = String.valueOf(extractSimbol3);
+        int forSumNumber3 = Integer.parseInt(valueSimbol3);
+        String forConvertNumber4 = String.valueOf(number);
+        char extractSimbol4 = forConvertNumber4.charAt(3);
+        String valueSimbol4 = String.valueOf(extractSimbol4);
+        int forSumNumber4 = Integer.parseInt(valueSimbol4);
+        int sumAllNumbers = forSumNumber1 + forSumNumber2 + forSumNumber3 + forSumNumber4;
+        System.out.println(sumAllNumbers);
     }
-
 }
